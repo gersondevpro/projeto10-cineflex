9d55270cd3f32a78b975916a59b5637da6be637c
GersonGoncalves <gerson.devpro@gmail.com>
2022-10-08 00:17:01 -0300

    Commit inicial do projeto

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import GlobalStyle from "./GlobalStyle"
+
+export default function App() {
+    return (
+        <>
+            <GlobalStyle/>
+            <h1>Testando aplicação</h1>
+        </>
+    )
+}
